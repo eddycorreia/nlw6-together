fa2c1a1561fa0033855009f9ccf4ce42dd3f8507
Edmilson Cardoso Correia <edmilson.cc@gmail.com>
2021-06-28 14:09:43 -0300

    aula 2 finalizada

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* abre e fecha o menu ao clicar no hamburguer ou no x */
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
@@ -6,3 +7,27 @@ for (const element of toggle) {
     nav.classList.toggle('show')
   })
 }
+
+/* Ao clicar no item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* Mudar o header da Pagina quando der scroll */
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    // scrool é maior ou igual a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
+  }
+})
